3406c8aeeb2ff5b781f578d77d4bbfe3b2e897fa
PrajyotG <prajyotgurav@gmail.com>
2020-10-23 17:12:46 +0530

    Added logic and some templates

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from string import ascii_lowercase, ascii_uppercase, punctuation, digits
+from random import choice
 
-# Create your views here.
+def home(request):
+    return render(request, "generator/home.html", {'to32':range(8,33)})
+
+
+def password(request):
+    pass_len = int(request.GET.get('length', 8))
+    chars = list(ascii_lowercase)
+    passw = ""
+
+    if request.GET.get('uppercase'):
+        chars.extend(list(ascii_uppercase))
+
+    if request.GET.get('digits'):
+        chars.extend(list(digits))
+
+    if request.GET.get('special'):
+        chars.extend(list(punctuation))
+
+    for i in range(pass_len):
+        passw += choice(chars)
+
+    return render(request, "generator/password.html", {'passw':passw})
+
+
+def about(request):
+    return render(request, "generator/about.html")
